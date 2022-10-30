b824dae2f40a61d451b13dc11b2cba2c8eeeae36
Yoruthemain <yorumain537@gmail.com>
2022-10-30 10:37:24 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 // equal to operator
-x=3
-if (x == 2){
+x=2
+if (x == '2'){
     console.log(x);
     // block of code to be executed...
 }
 
 //if the result is false no data will be run
+//if the result is false the else block of code will be executed
+if (x==3) {
+    console.log("this condition is true");
+} else {
+    console.log("this condtion is false");
+}
+
