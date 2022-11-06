d490e81caf5c9f5eb177da997648397ede9bb6b8
Yoruthemain <yorumain537@gmail.com>
2022-11-06 10:21:52 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // equal to operator
 x=2
-if (x == '2'){
+if (x == '2'){  
     console.log(x);
     // block of code to be executed...
 }
@@ -24,4 +24,40 @@ if (x!=2) {
     console.log("this condition is true");
 } else {
     console.log("this condition is false");
+}
+// This is not identical operator 
+if (x!=='2') { 
+    console.log("This condition is true and not identical");
+} else {
+    console.log("This condition is false and identical");
+    
+}
+//the greater than operator
+if (x>2) {
+    console.log("This condition is true and first value is greater than second value");
+    
+} else {
+    console.log("This condition is false and the first value is lesser than or equal to the second value ");
+    
+}
+// lesser than operator 
+if (x<3) {
+    console.log("this condition is true and the first value is less than the second value ");
+} else {
+    console.log("this conditons is false and the first value is greater than or equal to the second value ");
+}
+//this is greater than or equal to 
+if (x>=2) {
+    console.log("This condition returns true if first value greater than or equal to second value");
+    
+} else {
+    console.log("This conditon returns false if first value is lesser than second value ");
+}
+
+if (x<=1) {
+    console.log("if the first value is lesser or equal to the second value ");
+    
+} else {
+    console.log("if the first value is greater than the second value");
+    
 }
